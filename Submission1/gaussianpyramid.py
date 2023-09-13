2f988812cdd47b42fbf62fb78617341c109e753f
Isak Steinmo Hansen <89584305+isakhansen00@users.noreply.github.com>
2023-09-13 09:30:33 +0200

    Added text boxes to plot that shows the image size before and after applying gaussian pyramid

--- Submission1/gaussianpyramid.py
+++ Submission1/gaussianpyramid.py
@@ -9,12 +9,12 @@ lower = img.copy()
 
 # Create a Gaussian Pyramid
 gaussian_pyr = [lower]
-for i in range(3):
+for i in range(4):
    lower = cv.pyrDown(lower)
    gaussian_pyr.append(lower)
 
 
-figure, axs = plt.subplots(4, 2, figsize=(10, 15))
+figure, axs = plt.subplots(4, 2, figsize=(12, 9))
         
 # Create windows for each layer of the Gaussian pyramid
 for i, layer in enumerate(gaussian_pyr):
@@ -22,7 +22,8 @@ for i, layer in enumerate(gaussian_pyr):
         axs[i, 0].imshow(cv.cvtColor(layer, cv.COLOR_BGR2RGB))
         axs[i, 0].set_title(f'Original image')
         axs[i, 0].axis('off')
-
+    elif i == 4:
+        pass
     else:   
         axs[i, 0].imshow(cv.cvtColor(layer, cv.COLOR_BGR2RGB))
         axs[i, 0].set_title(f'Gaussian layer: {i}')
@@ -33,7 +34,7 @@ for i, layer in enumerate(gaussian_pyr):
 laplacian_top = gaussian_pyr[-1]
 lp = [laplacian_top]
 
-for i in range(3, 0, -1):
+for i in range(4, 0, -1):
    size = (gaussian_pyr[i - 1].shape[1], gaussian_pyr[i - 1].shape[0])
    gaussian_expanded = cv.pyrUp(gaussian_pyr[i], dstsize=size)
    laplacian = cv.subtract(gaussian_pyr[i-1], gaussian_expanded)
@@ -51,24 +52,35 @@ if len(gaussian_pyr) < 3:
 
 # Show the plot
 #plt.tight_layout()
-plt.show()
+
 
 
 # Code in block comment below is used for saving the compressed image and then reading out its new file size
-'''
+
 upscaled_image = gaussian_pyr[-1]
 for i in range(3):
     upscaled_image = cv.pyrUp(upscaled_image)
 
-cv.imshow("Upscaled to original", upscaled_image)
-cv.imwrite('test.png', gaussian_pyr[-1])
+cv.imwrite('test.png', gaussian_pyr[-2])
+original_image_size = os.stat('sui.png').st_size / 1000
 compressed_file_size = os.stat('test.png').st_size / 1000
-print(f"{compressed_file_size:.0f}kB")
-'''
 
-# # display all three layers
-# # cv.imshow('Layer 1',gaussian_pyr[3])
-# # cv.imshow('Layer 2',gaussian_pyr[2])
-# # cv.imshow("Layer 3", gaussian_pyr[1])
-# # cv.imshow("Original", gaussian_pyr[0])
-# cv.waitKey(0)
+figure.add_subplot(2, 2, 3)
+plt.axis('off')
+plt.text(0.5, -0.3, 'Original Size of Image :', weight="bold", fontsize = 18)
+
+figure.add_subplot(2, 2, 4)
+plt.axis('off')
+plt.text(0.2, -0.3,  f"{original_image_size:.0f}kB", fontsize = 18)
+
+# showing compression size in bits
+figure.add_subplot(2, 2, 3)
+plt.axis('off')
+plt.text(0.5, -0.15, 'Compressed Size :', weight="bold", fontsize = 18)
+
+figure.add_subplot(2, 2, 4)
+plt.axis('off')
+plt.text(0.2, -0.15, f"{compressed_file_size:.0f}kB" , fontsize = 18)
+
+
+plt.show()
