320a3fc8773a8de264430811b9c53beb3cae395c
Jørgen Nordås <rrrnord@hotmail.no>
2023-10-25 11:38:03 +0200

    made the code do what it is actually supposed to.

--- Submission2/task5/harnverhalt.py
+++ Submission2/task5/harnverhalt.py
@@ -1,119 +1,123 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from tkinter import *
 import os
 
-"""
---- Oppgave 5 - SIFT + Morphological operations "Harnverhalt" ---
-Denne oppgaven gikk ut på å feature matche ved hjelp av SIFT og å i tillegg kunne gjøre morphologiske
-operasjoner på bildeserien vi har fått utlevert. dette for å tydeligere se etter features på bildet.
-Programmet fungerer slik:
-Man starter programmet og blir møtt med en "meny" hvor man kan trykke tast 1, 2 eller 3.
-om man trykker "1" får man mulighet til å utføre "closing" på bildet, dvs. dilation og erotion etter hverandre.
-dette gjennomføres på samtlige bilder i bildeserien, og man får et gjennomsnittsresultat etter loopen er kjørt
+# Get the directory where the script is located
+image_dir = os.path.abspath(os.path.dirname(__file__))
 
-om man trykker "2" får man en SIFT-analysert utgave av bildeserien.
+# Create a GUI window
+root = Tk()
+root.title("ROI Detection")
 
-om man trykker "3" får man en kombinasjon av det morphologiske bildet og den sift analyserte utgaven som overlapper hverandre.
+# Create a label
+label = Label(root, text="Select Morphological Operation:")
+label.pack()
 
---- REFLEKSJON ---
-I etterkant ville det være bedre med færre keypoints i SIFT analysen, da så mange punkter gjøre det betydelig vanskeligere å se
-hva som faktisk er relevant og ikke.
-"""
+# Create a dropdown menu for morphological operations
+operations = ["None", "Closing", "Erosion", "Dilation"]
+operation_var = StringVar()
+operation_var.set(operations[0])  # Set the default option to "None"
+operation_menu = OptionMenu(root, operation_var, *operations)
+operation_menu.pack()
 
-image_dir = os.path.abspath(os.path.dirname(__file__))  # Assuming the script is in the same directory as the images
+# Create radio buttons for SIFT
+sift_var = IntVar()
+sift_checkbox = Checkbutton(root, text="Use SIFT", variable=sift_var)
+sift_checkbox.pack()
 
-# Function to detect ROIs using morphological operations
-def detect_rois_morphological(image):
-    # Convert the image to grayscale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
-    # Apply morphological operations (thresholding, erosion, and dilation)
-    _, binary = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 1))
-    binary = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, kernel)
-    
-    return binary
+# Function to load images and perform detection
+def detect_roi():
+    # Construct file paths for the template and image series using os.path.join and image_dir
+    template_path = os.path.join(image_dir, "matTeil.jpg")
+    template = cv2.imread(template_path, cv2.IMREAD_GRAYSCALE)
 
-# Function to detect ROIs using SIFT
-def detect_rois_sift(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    sift = cv2.SIFT_create()
-    keypoints, _ = sift.detectAndCompute(gray, None)
-    
-    return keypoints
+    # Get the selected morphological operation
+    morph_operation = operation_var.get()
 
-# Function to combine morphological and SIFT results
-def combine_morphological_and_sift(image, morphological_result, keypoints):
-    # Convert morphological result to BGR format
-    morphological_result_bgr = cv2.cvtColor(morphological_result, cv2.COLOR_GRAY2BGR)
-    
-    # Overlay keypoints on the morphological result
-    result = cv2.drawKeypoints(morphological_result_bgr, keypoints, image)
-    
-    return result
+    # Set a threshold for template matching
+    threshold = 0.65
 
-# Load the image series (harnverhalt1.jpg to harnverhalt14.jpg)
+    # Create a grid of plots
+    num_columns = 4  # Number of columns in the grid
+    num_rows = 4     # Number of rows in the grid
 
-image_series = [cv2.imread(os.path.join(image_dir, f'harnverhalt{i}.png'), cv2.IMREAD_COLOR) for i in range(1, 15)]
+    fig, axes = plt.subplots(num_rows, num_columns, figsize=(15, 15))
+    fig.suptitle("ROI Detection Results")
 
-# Create a menu to choose the operation
-print("Select the type of operation:")
-print("1. Morphological Operations")
-print("2. SIFT Feature Detection")
-print("3. Combination of Morphological and SIFT")
+    for i in range(1, 15):
 
-choice = int(input("Enter the operation number (1/2/3): "))
+        # Construct the file path for the current image using os.path.join and image_dir
+        image_filename = f"harnverhalt{i}.png"
+        image_path = os.path.join(image_dir, image_filename)
+        image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
-# Process the entire image series based on the chosen operation
-if choice == 1:
-    results = [detect_rois_morphological(image) for image in image_series]
-    result_name = 'Morphological'
-elif choice == 2:
-    keypoints_list = []
-    for image in image_series:
-        keypoints = detect_rois_sift(image)
-        keypoints_list.append(keypoints)
-    
-    # Combine all keypoints onto the first image
-    combined_image = image_series[0].copy()
-    for i in range(1, len(keypoints_list)):
-        combined_image = cv2.drawKeypoints(combined_image, keypoints_list[i], combined_image)
-    
-    result_name = 'SIFT'
-elif choice == 3:
-    results = []
-    for image in image_series:
-        morphological_result = detect_rois_morphological(image)
-        keypoints = detect_rois_sift(image)
-        combined_result = combine_morphological_and_sift(image, morphological_result, keypoints)
-        results.append(combined_result)
-    result_name = 'Combination of Morphological and SIFT'
+        # Apply the selected morphological operation
+        if morph_operation == "Closing":
+            kernel = np.ones((5, 5), np.uint8)
+            image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+        elif morph_operation == "Erosion":
+            kernel = np.ones((5, 5), np.uint8)
+            image = cv2.erode(image, kernel, iterations=1)
+        elif morph_operation == "Dilation":
+            kernel = np.ones((5, 5), np.uint8)
+            image = cv2.dilate(image, kernel, iterations=1)
+        elif morph_operation == "None":
+            pass  # No morphological operation
 
-# Display the results
-fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+        # If SIFT is selected, perform SIFT matching
+        if sift_var.get() == 1:
+            sift = cv2.SIFT_create()
+            kp1, des1 = sift.detectAndCompute(template, None)
+            kp2, des2 = sift.detectAndCompute(image, None)
 
-original_image_series = [cv2.imread(os.path.join(image_dir, f'harnverhalt{i}.png'), cv2.IMREAD_COLOR) for i in range(1, 15)]
-# Show the first image (original) on the left
-ax[0].imshow(original_image_series[0][:, :, ::-1])
-ax[0].set_title('Original Image')
-ax[0].axis('off')
+            # FLANN parameters
+            flann_index_kdtree = 0
+            index_params = dict(algorithm=flann_index_kdtree, trees=5)
+            search_params = dict(checks=50)
 
-if choice == 2:
-    # Display the combined SIFT result on the right
-    ax[1].imshow(combined_image[:, :, ::-1])
-    ax[1].set_title(f'Combined {result_name} SIFT Results')
-elif choice == 3:
-    # Display the combined result for Morphological and SIFT on the right
-    ax[1].imshow(results[0][:, :, ::-1])
-    ax[1].set_title(f'{result_name} Result for the First Image')
-else:
-    # Display the processed (average) image on the right
-    average_result = np.mean(results, axis=0).astype(np.uint8)
-    ax[1].imshow(average_result, cmap='gray' if result_name == 'Morphological' else None)
-    ax[1].set_title(f'Average {result_name} Result')
+            flann = cv2.FlannBasedMatcher(index_params, search_params)
+            matches = flann.knnMatch(des1, des2, k=2)
 
-ax[1].axis('off')
+            # Apply ratio test
+            good_matches = []
+            for m, n in matches:
+                if m.distance < 0.7 * n.distance:
+                    good_matches.append(m)
 
-plt.tight_layout()
-plt.show()
+            # Draw matched lines
+            img_matches = cv2.drawMatches(template, kp1, image, kp2, good_matches, None)
+            row, col = (i - 1) // num_columns, (i - 1) % num_columns
+            axes[row, col].imshow(img_matches)
+            axes[row, col].set_title(f"Similarity Lines - {image_filename}", fontsize=10)
+
+        # If SIFT is not selected, use template matching
+        else:
+            res = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
+            loc = np.where(res >= threshold)
+
+            image_color = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)  # Convert to color image
+
+            for pt in zip(*loc[::-1]):
+                cv2.rectangle(image_color, pt, (pt[0] + template.shape[1], pt[1] + template.shape[0]), (0, 255, 0), 2)
+
+            row, col = (i - 1) // num_columns, (i - 1) % num_columns
+            axes[row, col].imshow(image_color)
+            axes[row, col].set_title(f"ROI Detection for {image_filename}", fontsize=10)
+
+    # Hide empty subplots
+    for i in range(14, num_columns * num_rows):
+        row, col = i // num_columns, i % num_columns
+        fig.delaxes(axes[row, col])
+
+    plt.tight_layout()
+    plt.subplots_adjust(top=0.9)
+    plt.show()
+
+# Create a button to start detection
+detect_button = Button(root, text="Detect ROI", command=detect_roi)
+detect_button.pack()
+
+# Start the GUI loop
+root.mainloop()
