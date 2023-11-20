f11f7c121429e60d8a3633e5f26aa32d7c541563
Isak Steinmo Hansen <89584305+isakhansen00@users.noreply.github.com>
2023-11-20 10:03:21 +0100

    task3 and 4

--- Submission2/task3/task3.py
+++ Submission2/task3/task3.py
@@ -4,18 +4,25 @@ import pywt
 import pywt.data
 import cv2
 
-# Load an example image
 image_path = "sui.png"
 n=2
 original_image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
-coeffs = pywt.wavedec2(original_image, 'haar', level=n)
 
-coeffs[0] /= np.abs(coeffs[0]).max()
-for detail_level in range(n):
-    coeffs[detail_level + 1] = [d/np.abs(d).max() for d in 
-                                coeffs[detail_level + 1]]
-    
-array, coeff_slices = pywt.coeffs_to_array(coeffs)
-plt.imshow(array, cmap='gray', vmin=-0.25, vmax=0.75)
+coeffs2 = pywt.dwt2(original_image, 'haar')
+LL, (LH, HL, HH) = coeffs2
+
+
+titles = ['Approximation', ' Horizontal detail',
+          'Vertical detail', 'Diagonal detail']
+images = [LL, LH, HL, HH]
+
+fig = plt.figure(figsize=(12, 3))
+for i, (image, title) in enumerate(zip(images, titles)):
+    ax = fig.add_subplot(1, 4, i + 1)
+    ax.imshow(np.asarray(image), cmap=plt.cm.gray)
+    ax.set_title(title)
+    ax.axis('off')
+
+plt.tight_layout()
 plt.show()
--- Submission2/task4/task4.py
+++ Submission2/task4/task4.py
@@ -1,45 +1,100 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pywt
-import pywt.data
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import pywt
+# import pywt.data
+
+# import cv2
+
+# # Load an example image
+# image_path = "sui.png"
+# n=2
+# original_image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+# # Flatten the 2D image into a 1D array
+# original_signal = original_image.flatten()
+
+# # 1D Discrete Wavelet Transform (DWT)
+# coeffs = pywt.dwt(original_signal, 'db1')
+
+# # Choose a specific level to embed meta information
+# meta_level = 1
+
+# # Get the coefficients at the chosen level
+# coeffs_list = list(coeffs)
+
+# # Get the shape of the coefficients at the chosen level
+# coeffs_shape = coeffs_list[meta_level].shape
+
+# # Generate a binary signature (you can replace this with your own signature creation logic)
+# signature = np.random.randint(0, 25, coeffs_shape)
+
+# # Embed signature in the wavelet coefficients
+# coeffs_list[meta_level] = coeffs_list[meta_level] + signature
+
+# # Convert back to tuple
+# coeffs = tuple(coeffs_list)
+
+# # Inverse 1D Discrete Wavelet Transform (IDWT)
+# modified_signal = pywt.idwt(coeffs[0], coeffs[meta_level], 'db1')
+
+# # Reshape the modified signal back to the 2D image shape
+# modified_image = modified_signal.reshape(original_image.shape)
+
+# # Plot original and modified images
+# plt.figure(figsize=(10, 6))
+# plt.subplot(1, 2, 1)
+# plt.imshow(original_image, cmap='gray')
+# plt.title('Original Image')
+
+# plt.subplot(1, 2, 2)
+# plt.imshow(modified_image.astype('uint8'), cmap='gray')
+# plt.title('Modified Image with Signature')
+# plt.show()
+
 
 import cv2
+import pywt
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Load an example image
-image_path = "sui.png"
-n=2
-original_image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+def embed_meta_info(image, meta_info):
+    # Perform DWT on the image
+    coeffs = pywt.dwt2(image, 'haar')
 
-# Perform 2D wavelet transform (MRA) on the original image
-coeffs2 = pywt.dwt2(original_image, 'haar')
-LL, (LH, HL, HH) = coeffs2
+    # Embed meta information in the LL subband
+    LL_subband = coeffs[0]
+    LL_subband[:meta_info.shape[0], :meta_info.shape[1]] = meta_info
 
-# Define meta information (for example, a watermark)
-meta_info = np.random.randint(0, 128, size=LL.shape)  # Ensure meta_info has the same dimensions as LL
-print(meta_info)
+    # Reconstruct the image
+    reconstructed_image = pywt.idwt2(coeffs, 'haar')
 
+    return reconstructed_image
 
+def main():
+    image_path = "sui.png"
+    n=2
+    original_image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
-# Resize meta_info to match the shape of LL
-meta_info_resized = cv2.resize(meta_info, (LL.shape[1], LL.shape[0]))
+    # Create a small square as meta information
+    meta_info_size = 100
+    meta_info = np.ones((meta_info_size, meta_info_size), dtype=np.uint8)
+    print(meta_info)
 
-# Exchange the LL (approximation) coefficients with meta information
-LL_with_meta_info = LL + meta_info_resized
+    # Embed meta information
+    new_image = embed_meta_info(original_image, meta_info)
 
-# Reconstruct the image using the modified coefficients
-modified_image = pywt.idwt2((LL_with_meta_info, (LH, HL, HH)), 'haar')
+    # Plot the original and the new image
+    plt.figure(figsize=(8, 4))
 
-# Plot the original and modified images
-plt.figure(figsize=(12, 6))
-plt.subplot(1, 2, 1)
-plt.imshow(original_image, cmap='gray')
-plt.title('Original Image')
-plt.axis('off')
+    plt.subplot(1, 2, 1)
+    plt.imshow(original_image, cmap='gray')
+    plt.title('Original Image')
 
-plt.subplot(1, 2, 2)
-plt.imshow(modified_image, cmap='gray')
-plt.title('Modified Image with Meta Information')
-plt.axis('off')
+    plt.subplot(1, 2, 2)
+    plt.imshow(new_image, cmap='gray')
+    plt.title('Image with Embedded Meta Info')
 
-plt.tight_layout()
-plt.show()
+    plt.show()
+
+if __name__ == "__main__":
+    main()
